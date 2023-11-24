1887afaa3d50e13807f7a89c9f8f5a2e47521a1a
tjakopecffos <tjakopec@ffos.hr>
2023-11-24 06:11:15 +0000

    Testing commit

--- test-project/src/main/java/com/mycompany/app/App.java
+++ test-project/src/main/java/com/mycompany/app/App.java
@@ -5,13 +5,11 @@
 
 package com.mycompany.app;
 
-import static javax.xml.XMLConstants.XML_NS_PREFIX;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Remote World!" );
-        System.out.println("The XML namespace prefix is: " + XML_NS_PREFIX);
+        System.out.println( "Hello FFOS" );
     }
 }
